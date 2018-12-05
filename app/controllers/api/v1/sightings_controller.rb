f8cd021fa4ead66b37b9c12dd034507954d596c4
brewchetta <chett.tiller@gmail.com>
2018-12-05 14:29:04 -0500

    Added create functionality

--- app/controllers/api/v1/sightings_controller.rb
+++ app/controllers/api/v1/sightings_controller.rb
@@ -10,4 +10,26 @@ class Api::V1::SightingsController < ApplicationController
     render json: @sighting, status: :ok
   end
 
+  def create
+    @sighting = Sighting.create(sighting_params)
+    if @sighting.valid?
+      render json: @sighting, status: :ok
+    else
+      render json: @sighting.errors.full_messages, status: :not_acceptable
+    end
+  end
+  # End create
+
+  def update
+    @sighting = Sighting.update(sighting_params)
+    render json: @sighting, status: :ok
+  end
+  # End update
+
+  private
+
+  def sighting_params
+    params.require(:sighting).permit(:entity, :lat, :long, :description, :image, :user_id)
+  end
+
 end
